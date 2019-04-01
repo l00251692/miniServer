eee2ff0282a94da800825294ef4b327f52dcf1d1
l00251692 <982679298@qq.com>
2019-04-01 22:15:30 +0800

    【功能】提交动态设置数据源测试代码，用的临时appId,先解决启动问题

--- paascloud-provider/paascloud-provider-uac/src/main/java/com/paascloud/provider/security/PcAuthenticationSuccessHandler.java
+++ paascloud-provider/paascloud-provider-uac/src/main/java/com/paascloud/provider/security/PcAuthenticationSuccessHandler.java
@@ -2,6 +2,7 @@ package com.paascloud.provider.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.paascloud.core.utils.RequestUtil;
+import com.paascloud.provider.config.DataSourceHolder;
 import com.paascloud.provider.service.UacUserService;
 import com.paascloud.security.core.SecurityUser;
 import com.paascloud.wrapper.WrapMapper;
@@ -78,6 +79,7 @@ public class PcAuthenticationSuccessHandler extends SavedRequestAwareAuthenticat
 
 		OAuth2AccessToken token = authorizationServerTokenServices.createAccessToken(oAuth2Authentication);
 		SecurityUser principal = (SecurityUser) authentication.getPrincipal();
+		DataSourceHolder.setDataSource("exampleId1234");
 		uacUserService.handlerLoginData(token, principal, request);
 
 		log.info("用户【 {} 】记录登录日志", principal.getUsername());
--- paascloud-provider/paascloud-provider-uac/src/main/java/com/paascloud/provider/security/UacUserDetailsServiceImpl.java
+++ paascloud-provider/paascloud-provider-uac/src/main/java/com/paascloud/provider/security/UacUserDetailsServiceImpl.java
@@ -1,5 +1,6 @@
 package com.paascloud.provider.security;
 
+import com.paascloud.provider.config.DataSourceHolder;
 import com.paascloud.provider.model.domain.UacUser;
 import com.paascloud.provider.service.UacUserService;
 import com.paascloud.security.core.SecurityUser;
@@ -33,10 +34,12 @@ public class UacUserDetailsServiceImpl implements UserDetailsService {
 	@Override
 	public UserDetails loadUserByUsername(String username) {
 		Collection<GrantedAuthority> grantedAuthorities;
+		DataSourceHolder.setDataSource("exampleId1234");
 		UacUser user = uacUserService.findByLoginName(username);
 		if (user == null) {
 			throw new BadCredentialsException("用户名不存在或者密码错误");
 		}
+		
 		user = uacUserService.findUserInfoByUserId(user.getId());
 		grantedAuthorities = uacUserService.loadUserAuthorities(user.getId());
 		return new SecurityUser(user.getId(), user.getLoginName(), user.getLoginPwd(),
