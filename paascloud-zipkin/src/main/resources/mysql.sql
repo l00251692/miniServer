15226cfbb5b9d88f6e245a4809a9987ec5748e6b
jhcy55 <jhcy55@126.com>
2019-10-15 15:28:09 +0800

    zipkin sql修改

--- paascloud-zipkin/src/main/resources/mysql.sql
+++ paascloud-zipkin/src/main/resources/mysql.sql
@@ -6,14 +6,13 @@ CREATE TABLE IF NOT EXISTS zipkin_spans (
   `parent_id` BIGINT,
   `debug` BIT(1),
   `start_ts` BIGINT COMMENT 'Span.timestamp(): epoch micros used for endTs query and to implement TTL',
-  `duration` BIGINT COMMENT 'Span.duration(): micros used for minDuration and maxDuration query'
+  `duration` BIGINT COMMENT 'Span.duration(): micros used for minDuration and maxDuration query',
+  UNIQUE KEY(`trace_id_high`, `trace_id`, `id`) COMMENT 'ignore insert on duplicate',
+  INDEX(`trace_id_high`, `trace_id`, `id`) COMMENT 'for joining with zipkin_annotations',
+  INDEX(`trace_id_high`, `trace_id`) COMMENT 'for getTracesByIds',
+  INDEX(`name`) COMMENT 'for getTraces and getSpanNames',
+  INDEX(`start_ts`) COMMENT 'for getTraces ordering and range'
 ) ENGINE=InnoDB ROW_FORMAT=COMPRESSED CHARACTER SET=utf8 COLLATE utf8_general_ci;
-
-ALTER TABLE zipkin_spans ADD UNIQUE KEY(`trace_id_high`, `trace_id`, `id`) COMMENT 'ignore insert on duplicate';
-ALTER TABLE zipkin_spans ADD INDEX(`trace_id_high`, `trace_id`, `id`) COMMENT 'for joining with zipkin_annotations';
-ALTER TABLE zipkin_spans ADD INDEX(`trace_id_high`, `trace_id`) COMMENT 'for getTracesByIds';
-ALTER TABLE zipkin_spans ADD INDEX(`name`) COMMENT 'for getTraces and getSpanNames';
-ALTER TABLE zipkin_spans ADD INDEX(`start_ts`) COMMENT 'for getTraces ordering and range';
 
 CREATE TABLE IF NOT EXISTS zipkin_annotations (
   `trace_id_high` BIGINT NOT NULL DEFAULT 0 COMMENT 'If non zero, this means the trace uses 128 bit traceIds instead of 64 bit',
@@ -26,22 +25,23 @@ CREATE TABLE IF NOT EXISTS zipkin_annotations (
   `endpoint_ipv4` INT COMMENT 'Null when Binary/Annotation.endpoint is null',
   `endpoint_ipv6` BINARY(16) COMMENT 'Null when Binary/Annotation.endpoint is null, or no IPv6 address',
   `endpoint_port` SMALLINT COMMENT 'Null when Binary/Annotation.endpoint is null',
-  `endpoint_service_name` VARCHAR(255) COMMENT 'Null when Binary/Annotation.endpoint is null'
+  `endpoint_service_name` VARCHAR(255) COMMENT 'Null when Binary/Annotation.endpoint is null',
+  UNIQUE KEY(`trace_id_high`, `trace_id`, `span_id`, `a_key`, `a_timestamp`) COMMENT 'Ignore insert on duplicate',
+  INDEX(`trace_id_high`, `trace_id`, `span_id`) COMMENT 'for joining with zipkin_spans',
+  INDEX(`trace_id_high`, `trace_id`, `span_id`) COMMENT 'for joining with zipkin_spans',
+  INDEX(`trace_id_high`, `trace_id`) COMMENT 'for getTraces/ByIds',
+  INDEX(`endpoint_service_name`) COMMENT 'for getTraces and getServiceNames',
+  INDEX(`a_type`) COMMENT 'for getTraces',
+  INDEX(`a_key`) COMMENT 'for getTraces',
+  INDEX(`trace_id`, `span_id`, `a_key`) COMMENT 'for dependencies job'
 ) ENGINE=InnoDB ROW_FORMAT=COMPRESSED CHARACTER SET=utf8 COLLATE utf8_general_ci;
 
-ALTER TABLE zipkin_annotations ADD UNIQUE KEY(`trace_id_high`, `trace_id`, `span_id`, `a_key`, `a_timestamp`) COMMENT 'Ignore insert on duplicate';
-ALTER TABLE zipkin_annotations ADD INDEX(`trace_id_high`, `trace_id`, `span_id`) COMMENT 'for joining with zipkin_spans';
-ALTER TABLE zipkin_annotations ADD INDEX(`trace_id_high`, `trace_id`) COMMENT 'for getTraces/ByIds';
-ALTER TABLE zipkin_annotations ADD INDEX(`endpoint_service_name`) COMMENT 'for getTraces and getServiceNames';
-ALTER TABLE zipkin_annotations ADD INDEX(`a_type`) COMMENT 'for getTraces';
-ALTER TABLE zipkin_annotations ADD INDEX(`a_key`) COMMENT 'for getTraces';
-ALTER TABLE zipkin_annotations ADD INDEX(`trace_id`, `span_id`, `a_key`) COMMENT 'for dependencies job';
 
 CREATE TABLE IF NOT EXISTS zipkin_dependencies (
   `day` DATE NOT NULL,
   `parent` VARCHAR(255) NOT NULL,
   `child` VARCHAR(255) NOT NULL,
-  `call_count` BIGINT
+  `call_count` BIGINT,
+  UNIQUE KEY(`day`, `parent`, `child`)
 ) ENGINE=InnoDB ROW_FORMAT=COMPRESSED CHARACTER SET=utf8 COLLATE utf8_general_ci;
 
-ALTER TABLE zipkin_dependencies ADD UNIQUE KEY(`day`, `parent`, `child`);
